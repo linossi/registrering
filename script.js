0b640c43d8a15fcc72b3f33c2e343d10e71e582a
Linossi <145012929+linossi@users.noreply.github.com>
2024-05-06 00:44:40 +0200

    save

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-let handlekurv = {};
+let totalSum = 0;
 
-function leggTilIVarekurv(vareNavn, pris) {
-    if (handlekurv[vareNavn]) {
-        handlekurv[vareNavn].antall++;
-    } else {
-        handlekurv[vareNavn] = {
-            pris: pris,
-            antall: 1
-        };
-    }
-    oppdaterHandlekurv();
+function addToCart(price) {
+    totalSum += price;
+    alert("Produktet er lagt til i handlekurven!");
 }
 
-function oppdaterHandlekurv() {
-    const handlekurvElementer = document.getElementById('handlekurv');
-    const handlekurvTotal = document.getElementById('handlekurv-totalt');
-    handlekurvElementer.innerHTML = '';
-    let totaltBeløp = 0;
-    for (const vareNavn in handlekurv) {
-        const { pris, antall } = handlekurv[vareNavn];
-        const vareTotal = pris * antall;
-        totaltBeløp += vareTotal;
-        const vareElement = document.createElement('div');
-        vareElement.classList.add('handlevare');
-        vareElement.textContent = `${antall} x ${vareNavn} (${pris.toFixed(2)} kr)`;
-        handlekurvElementer.appendChild(vareElement);
-    }
-    handlekurvTotal.textContent = `Totalpris: ${totaltBeløp.toFixed(2)} kr`;
-    document.getElementById('handlekurv').style.display = 'block';
+window.onload = function() {
+    document.querySelector("header").innerHTML += "<div id='cart'><h2>Handlekurv</h2><p>Total sum: $<span id='totalSum'>0</span></p></div>";
+    updateTotalSum();
+};
+
+function updateTotalSum() {
+    document.getElementById("totalSum").innerText = totalSum;
 }
