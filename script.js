1ab955f09d0222bb325ac012219e94476d50ca82
Linossi <145012929+linossi@users.noreply.github.com>
2024-05-06 00:49:26 +0200

    save

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ let totalSum = 0;
 
 function addToCart(price) {
     totalSum += price;
-    alert("Produktet er lagt til i handlekurven!");
-}
-
-window.onload = function() {
-    document.querySelector("header").innerHTML += "<div id='cart'><h2>Handlekurv</h2><p>Total sum: $<span id='totalSum'>0</span></p></div>";
     updateTotalSum();
-};
+}
 
 function updateTotalSum() {
     document.getElementById("totalSum").innerText = totalSum;
 }
+
