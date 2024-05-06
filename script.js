220f1d73e1c74f95dfec40623f886ece156566b3
Linossi <145012929+linossi@users.noreply.github.com>
2024-05-06 02:45:15 +0200

    save

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 let totalSum = 0;
 let cartItems = [];
 
-function addToCart(price) {
-    cartItems.push(price);
+function addToCart(productName, price) {
+    cartItems.push(productName);
     updateCart();
 }
 
 function updateCart() {
     const cartList = document.getElementById("cartItems");
     cartList.innerHTML = "";
-    totalSum = 0; // Nullstiller totalsummen
     cartItems.forEach((item, index) => {
         const listItem = document.createElement("li");
-        listItem.textContent = "Produkt " + (index + 1) + ": $" + item;
+        listItem.textContent = item;
         const removeButton = document.createElement("button");
         removeButton.textContent = "Fjern";
         removeButton.onclick = function() {
-            totalSum -= cartItems[index];
             cartItems.splice(index, 1);
             updateCart();
         };
         listItem.appendChild(removeButton);
         cartList.appendChild(listItem);
-        totalSum += item; // Oppdaterer totalsummen
     });
 }
 
@@ -30,5 +27,8 @@ function calculateTotal() {
     document.getElementById("totalValue").innerText = totalSum;
     document.getElementById("totalBox").classList.remove("hidden");
 }
+
+
+
 
 
